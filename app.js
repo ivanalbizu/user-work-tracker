19c55de7292c80a3f74f4566773e5684411f5893
ivanalbizu <ivan.albizu@gmail.com>
2020-05-01 19:18:35 +0200

    beautifier js

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const { checkToken } = require('./helpers/auth')
 
-let unless = function(middleware, ...paths) {
-    return function(req, res, next) {
-        const pathCheck = paths.some(path => path === req.path)
-        pathCheck ? next() : middleware(req, res, next)
-    }
+let unless = (middleware, ...paths) => {
+  return (req, res, next) => {
+    const pathCheck = paths.some(path => path === req.path)
+    pathCheck ? next() : middleware(req, res, next)
+  }
 }
 
 app.use(morgan('tiny'))
 app.use(cookieParser())
-app.use(bodyParser.json({limit: '10mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+app.use(bodyParser.json({
+  limit: '10mb',
+  extended: true
+}))
+app.use(bodyParser.urlencoded({
+  limit: '10mb',
+  extended: true
+}))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
--- helpers/fspromises.js
+++ helpers/fspromises.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
+const fs = require('fs')
 
 // Reads text from the file asynchronously.
 // Returns a Promise.
-function readPromise(file) {
+const readPromise = file => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, "utf8", (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
 // Writes given text to a file asynchronously.
 // Returns a Promise.
-function writePromise(file, data) {
+const writePromise = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, err => {
-      if (err) reject(err);
-      resolve('success');
+      if (err) reject(err)
+      resolve('success')
     });
   });
 }
