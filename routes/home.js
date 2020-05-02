a351e5eed11e6b3f0d3fd3367c3b13b9046bdfa6
ivanalbizu <ivan.albizu@gmail.com>
2020-05-02 18:41:25 +0200

    Home styles

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  res.render('main', {
+  res.render('home', {
     head_title: 'Home user Tracking',
     page_title: 'Home',
   })
